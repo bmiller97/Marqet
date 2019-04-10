76adf747a4e45c1e99170a25bf16c099854cb390
Will Youman <william.youman@marquette.edu>
2019-04-10 13:16:48 -0500

    new user link

--- booklisting.php
+++ booklisting.php
@@ -1,6 +1,6 @@
 <?php
-$connect = mysql_connect(“server_name”, “admin_name”, “password”); if (!connect) { die('Connection Failed: ' . mysql_error()); { mysql_select_db(“database_name”, $connect);
-
+$connect = mysql_connect(“”, “admin_name”, “password”); if (!connect) { die('Connection Failed: ' . mysql_error());
+{ mysql_select_db(“database_name”, $connect);
 
 $book_info = "INSERT INTO table_name(name, price, description)VALUES('$_POST[name]','$_POST[price]')",
 if(!mysql_query($user_info,$connect))
