188a6ebcf27f820746328f874489a1d5b4d2f577
Will Youman <william.youman@marquette.edu>
2019-04-14 16:29:04 -0500

    updates for sql

--- newbooklisting.js
+++ newbooklisting.js
@@ -10,7 +10,8 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "INSERT INTO books (Item Name, Price, Description, Contact) VALUES ?";cocon.query(sql, [values], function(err, result){
+  var sql = "INSERT INTO books (Item Name, Price, Description, Contact) VALUES ?";
+  cocon.query(sql, [values], function(err, result){
 if (err) throw err;
 });
 });
