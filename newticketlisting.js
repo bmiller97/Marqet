5113f25cd2d515311776d4f28a70a559a0e343a8
Will Youman <william.youman@marquette.edu>
2019-04-30 22:13:19 -0500

    fire

--- newticketlisting.js
+++ newticketlisting.js
@@ -15,6 +15,12 @@ con.connect(function(err) {
 if (err) throw err;
 });
 });*/
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
 var config = {
     apiKey: "AIzaSyDTsFKTGUHqQ4mYGGomFmcDyDpP781DaHs",
     authDomain: "marqet-1df05.firebaseapp.com",
