3dabd8ab0b07e81535de65e1036a465f0aa5d0ed
willyouman <46760317+willyouman@users.noreply.github.com>
2019-04-30 20:48:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+  var config = {
+    apiKey: "AIzaSyDTsFKTGUHqQ4mYGGomFmcDyDpP781DaHs",
+    authDomain: "marqet-1df05.firebaseapp.com",
+    databaseURL: "https://marqet-1df05.firebaseio.com",
+    projectId: "marqet-1df05",
+    storageBucket: "marqet-1df05.appspot.com",
+    messagingSenderId: "988423971651"
+  };
+  firebase.initializeApp(config);
 
